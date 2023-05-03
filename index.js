6755c38245bb721ef0c47b5a2bb721d953913cea
IH Shadin <shadinfr@yahoo.com>
2023-05-03 20:24:58 +0600

    sign chef api create

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const chefs = require('./data/chefs.json');
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Dragon is running');
+    res.send('Chefs are cooking');
 })
 app.get('/chefs', (req, res) => {
     res.send(chefs);
+})
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const seletedChef = chefs.find(chef => chef.id === id);
+    res.send(seletedChef);
 })
 app.listen(port, () => {
     console.log('Dragon API is running on port', port);
