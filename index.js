a45ebb3e226ffa654760e2627dda51fc41fe462a
IH Shadin <shadinfr@yahoo.com>
2023-05-03 13:38:26 +0600

    create api

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5100
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
     res.send('Dragon is running');
+})
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
 app.listen(port, () => {
     console.log('Dragon API is running on port', port);
